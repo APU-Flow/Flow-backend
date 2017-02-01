951133fc82053dccad141b3654fce12b0dd105ce
George Vine <george.h.vine.iv@gmail.com>
2017-02-01 13:26:19 -0800

    changed GETS to POSTS and updated config, dependencies

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 var express = require('express')
-var MongoClient = require('mongodb').MongoClient
 var app = express()
-var dburl = 'mongodb://localhost:27017/flow'
-app.listen(3000, function () {
-  console.log('FLOW listening on port 3000!')
-})
+var port = 3000
+var bodyParser = require('body-parser');
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
+app.listen(port);
 
 //here are some routes
-app.route('/api/usageEvent').post(function(req, res){
+app.post('/api/usageEvent', function(req, res){
   console.log(req)
   res.send('You sent: ${body} to Express')
 
@@ -38,8 +42,7 @@ app.route('/api/usageEvent').post(function(req, res){
 })
 
 
-app.route('/api/newUser').post(function(req, res){
-  /*
+app.post('/api/newUser', function(req, res){
   var firstName = req.body.firstName;
   var lastName = req.body.lastName;
   var streetAddress = req.body.streetAddress;
@@ -53,29 +56,14 @@ app.route('/api/newUser').post(function(req, res){
   console.log(city);
   console.log(state);
   console.log(email);
-  console.log(password);
-  */
-  console.log(req.body)
-})
+  console.log(password)
+});
 
 
 
-app.route('/api/login').post(function(req, res){
+app.post('/api/login', function(req, res){
   var email = req.param('email')
   var password = req.param('password')
   console.log("The user @ " + email + " attempted to log in")
   //todo: actual things
-})
-
-
-MongoClient.connect(dburl, function(err, db) {
-  if (err) return
-
-  var collection = db.collection('foods')
-  collection.insert({name: 'taco', tasty: true}, function(err, result) {
-    collection.find({name: 'taco'}).toArray(function(err, docs) {
-      console.log(docs[0])
-      db.close()
-    })
-  })
-})
+});
