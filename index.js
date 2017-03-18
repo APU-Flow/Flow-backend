6a4cfdfd9682fdbc915b3e63e93ba8fe86087024
Jonathan Ming <jming422@gmail.com>
2017-03-18 15:51:41 -0700

    First attempt at JWT token authentication

--- index.js
+++ index.js
@@ -1,26 +1,54 @@
-var _ = require("lodash");
-var express = require('express');
-var app = express();
-var port = 3000;
-var bodyParser = require('body-parser');
-var date = new Date();
-var url = "mongodb://localhost:27017/flow";
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
-app.use( bodyParser.json() );
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.listen(port);
+let _ = require("lodash");
+let express = require('express');
+let app = express();
+let port = 3000;
+let MongoClient = require('mongodb').MongoClient;
+let assert = require('assert');
+
+let bodyParser = require('body-parser');
+let jwt = require('jsonwebtoken');
+
+let config = require('./config'); // Get our config file
+app.set('uberSecret', config.secret);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 // get an instance of the router for api routes
-var apiRoutes = express.Router();
+let apiRoutes = express.Router();
 app.use('/api', apiRoutes);
+
+// Route middleware to verify a token
+apiRoutes.use(function(req, res, next) {
+
+  // Check header, url parameters, and post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  if (token) {
+    // Verify the secret and check the expiry
+    jwt.verify(token, app.get('uberSecret'), function(err, decoded) {      
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });    
+      } else {
+        // If everything is good, save the decoded token to the request for use in other routes
+        req.decoded = decoded;    
+        next();
+      }
+    });
+  } else {
+    // If there is no token, return an error
+    return res.status(403).send({ 
+        success: false, 
+        message: 'No token provided.' 
+    });
+  }
+});
 
 app.post('/login', function(req, res){
   res.setHeader('Content-Type', 'application/json');
-  var emailVal = req.param('email')
-  var passwordVal = req.param('password')
+  let emailVal = req.param('email')
+  let passwordVal = req.param('password')
   console.log("");
   console.log("");
   console.log("");
@@ -30,13 +58,22 @@ app.post('/login', function(req, res){
   console.log(new Date())
   //res.send("You attempted to login with email: " + emailVal + " and password: " + passwordVal)
 
-  MongoClient.connect(url, function(err, db){
+  MongoClient.connect(config.database, function(err, db){
     assert.equal(null, err);
     db.collection('users').find({email: emailVal}).toArray(function (err, result){
       jsonBody = result[0]
-      if(passwordVal == jsonBody["password"]){
-       // res.send(jsonBody)
-        res.json({message: "ok", email: emailVal});
+      // If our user is authenticated successfully:
+      if (passwordVal == jsonBody["password"]){
+        
+        let token = jwt.sign(user, app.get('uberSecret'), {
+          expiresInMinutes: 1440 // Expires in 24 hours
+        });
+
+        res.json({
+          message: "ok",
+          firstName: jsonBody["firstName"],
+          token
+        });
       } else {
         res.json({message: "lol nice tri n00b"});
       }
@@ -47,13 +84,13 @@ app.post('/login', function(req, res){
 
 
 app.post('/newUser', function(req, res){
-  var firstName = req.body.firstName;
-  var lastName = req.body.lastName;
-  var streetAddress = req.body.streetAddress;
-  var city = req.body.city;
-  var state = req.body.state;
-  var email = req.body.email;
-  var password = req.body.password;
+  let firstName = req.body.firstName;
+  let lastName = req.body.lastName;
+  let streetAddress = req.body.streetAddress;
+  let city = req.body.city;
+  let state = req.body.state;
+  let email = req.body.email;
+  let password = req.body.password;
 
   console.log("");
   console.log("");
@@ -69,7 +106,7 @@ app.post('/newUser', function(req, res){
   console.log(password)
   console.log(new Date());
 
-  MongoClient.connect(url, function(err, db){
+  MongoClient.connect(config.database, function(err, db){
     assert.equal(null, err);
     insertUser(db, function(){db.close()},
       firstName, lastName, streetAddress, city, state, email, password
@@ -82,14 +119,14 @@ app.post('/newUser', function(req, res){
 
 
 //here are some protected routes
-app.post('/api/usageEvent', function(req, res){
+apiRoutes.post('/usageEvent', function(req, res){
   console.log(req)
   res.send('You sent a usageEvent to Express')
 
-  var idVal = req.body.id;
-  var startTimeVal = req.body.startTime
-  var endTimeVal = req.body.endTime
-  var totalVolumeVal = req.body.totalVolume
+  let idVal = req.body.id;
+  let startTimeVal = req.body.startTime
+  let endTimeVal = req.body.endTime
+  let totalVolumeVal = req.body.totalVolume
   console.log("");
   console.log("");
   console.log("");
@@ -103,10 +140,10 @@ app.post('/api/usageEvent', function(req, res){
   console.log(new Date())
   res.send("New usage event logged");
   /*
-  MongoClient.connect(dburl, function(err, db) {
+  MongoClient.connect(config.database, function(err, db) {
     if (err) return
 
-    var collection = db.collection('flow')
+    let collection = db.collection('flow')
     collection.insert({id: idVal, startTime: startTimeVal, endTime: endTimeVal, totalVolume: totalVolumeVal}, function(err, result) {
       collection.find({id: '1234'}).toArray(function(err, docs) {
         console.log(docs[0])
@@ -118,14 +155,14 @@ app.post('/api/usageEvent', function(req, res){
 })
 
 
-app.get('/api/getUsageEvent', function(req, res){
+apiRoutes.get('/getUsageEvent', function(req, res){
   emailVal = req.param("email");
   console.log("")
   console.log("")
   console.log("")
   console.log("____________________")
   console.log("Usage event for " + emailVal + "pulled");
-  MongoClient.connect(url, function(err, db){
+  MongoClient.connect(config.database, function(err, db){
     assert.equal(null, err);
     db.collection('events').find({email: emailVal}).toArray(function (err, result){
       jsonBody = result[0]
@@ -136,7 +173,7 @@ app.get('/api/getUsageEvent', function(req, res){
 
 
 //HERE ARE SOME HELPER FUNCTIONS
-var insertUser = function(db, callback, firstName, lastName, streetAddress, city, state, email, password){
+let insertUser = function(db, callback, firstName, lastName, streetAddress, city, state, email, password){
   db.collection('users').insertOne({
     "firstName": firstName,
     "lastName": lastName,
@@ -151,3 +188,6 @@ var insertUser = function(db, callback, firstName, lastName, streetAddress, city
     callback();
   })
 }
+
+app.listen(port);
+console.log(`Flow-backend server running on port ${port}`);
