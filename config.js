3f9a1d1f7257460e4471855e9d8d09dc7242521e
Jonathan Ming <jming422@gmail.com>
2017-03-22 10:29:58 -0700

    Replaced JSHint with ESLint
    ESLint works with React more easily, and I don't want to have to deal with two different linters for our different Flow projects

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    'secret': 'supercoolflowsecretyeah',
-    'database': 'mongodb://localhost:27017/flow'
+  'secret': 'supercoolflowsecretyeah',
+  'database': 'mongodb://localhost:27017/flow'
 };
