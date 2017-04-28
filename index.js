2483a41680a7a1a74c0cc93c6bbd7b9a0135a413
George Vine <george.h.vine.iv@gmail.com>
2017-04-28 13:27:44 -0700

    Started using the async module for asyncronous witchcraft

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const assert = require('assert');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const bcrypt  = require('bcrypt');
+const async = require('async');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -104,24 +105,40 @@ app.post('/newUser', function(req, res) {
   // Destructure new user fields from request body into individual variables
   let {firstName, lastName, streetAddress, city, state, email, password} = req.body;
   email = email.toLowerCase();
-  MongoClient.connect(config.database, function(err, db) {
-    assert.equal(null, err);
-    let insertCompleted = false;
-    db.collection('users').count({ email }, function(err, count) {
-      if (count !== 0) {
-        res.status(409).json({message: `This email adress: ${email} is already registered to a user.`});
-        db.close();
-      } else {
-        console.log('\n-------------------------');
-        console.log(`New user registered: ${email}`);
-        console.log(firstName);
-        console.log(lastName);
-        console.log(city);
-        console.log(state);
-        console.log(email);
-        console.log(new Date());
-        insertCompleted = true;
+  let newUser = false;
+  let calls = [];
+  calls.push(function() {
+    MongoClient.connect(config.database, function(err, db) {
+      assert.equal(null, err);
+      db.collection('users').count({ email }, function(err, count) {
+        if (count !== 0) {
+          res.status(409).json({message: `This email adress: ${email} is already registered to a user.`});
+          db.close();
+        } else {
+          console.log('\n-------------------------');
+          console.log(`New user registered: ${email}`);
+          console.log(firstName);
+          console.log(lastName);
+          console.log(city);
+          console.log(state);
+          console.log(email);
+          console.log(new Date());
+          newUser = true;
+          db.close();
+        }
+      });
+    });
+  });
 
+  async.parallel(calls, function(err, result) {
+    /* this code will run after all calls finished the job or
+       when any of the calls passes an error */
+    if (err) {
+      return console.log(err);
+    }
+    if (newUser) {
+      MongoClient.connect(config.database, function(err, db) {
+        assert.equal(null, err);
         bcrypt.genSalt(config.saltRounds, function(err, salt) {
           bcrypt.hash(password, salt, function(err, hash) {
             assert.equal(null, err);
@@ -136,15 +153,13 @@ app.post('/newUser', function(req, res) {
             }); // End insertOne for the new user
           }); // End password hash
         }); // End saltGen
-      }
-    }); // End user count
-    if (insertCompleted == true) {
-      assert.equal(err, null);
-      res.json({userEmail: email });
-      console.log('Inserted user into db');
-      db.close();
+        assert.equal(err, null);
+        res.json({userEmail: email });
+        console.log('Inserted user into db');
+        db.close();
+      }); // End connect
     }
-  }); // End MongoClient connection
+  });
 }); // End route
 
 
